9d40cba8a187514b2cc553fa2f943cc9632f2abb
Amarganarada <131906487+EduardaMargonar06@users.noreply.github.com>
2025-09-03 21:29:15 -0300

    Create 35.c

---
+++ ATP_II/LISTA 1/35.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+    
+   int x = atoi(argv[1]);
+   if(x%2 == 0){
+       printf("é par");
+   }else{
+       printf("é impar");
+   }
+       
+    
+    
+}
