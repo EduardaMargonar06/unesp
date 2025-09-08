b0acf63bc43c35c698cf6f55e71a30671e589be3
Amarganarada <131906487+EduardaMargonar06@users.noreply.github.com>
2025-09-08 11:09:30 -0300

    Update 29.c

--- ATP_II/LISTA 1/29.c
+++ ATP_II/LISTA 1/29.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <ctype.h> /* Para a função isupper()*/
+#include <ctype.h>
 #include <limits.h>
-#include <stdbool.h> /* para poder usar o true e o false*/
-#include <string.h> /*manipulação de strings*/
-
+#include <stdbool.h> 
+#include <string.h> 
 int main(){
   char *array_de_pont[5];
-  char buffer[50]; /*temporario */
-
+  char buffer[50]; 
   for(int i=0;i<5;i++){
       printf("Digite o %d: ", i+1);
       fgets(buffer, sizeof(buffer), stdin);
@@ -22,6 +20,6 @@ int main(){
   printf("\nLista de palavras digitadas:\n");
     for (int i = 0; i < 5; i++) {
         printf("%s;", array_de_pont[i]);
-        free(array_de_pont[i]); // Libera a memória
+        free(array_de_pont[i]); 
     }
 }
