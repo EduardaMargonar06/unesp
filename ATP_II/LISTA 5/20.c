a60eda39fcf841ca34358286375ce60362161c95
Amarganarada <131906487+EduardaMargonar06@users.noreply.github.com>
2025-09-08 17:36:44 -0300

    Create 20.c

---
+++ ATP_II/LISTA 5/20.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int raiz(int n, int baixo, int alto){
+    if(baixo > alto) return alto;
+    int meio = (baixo+alto)/2;
+    if(meio*meio == n) return meio;
+    if(meio*meio > n) return raiz(n, baixo, meio-1);
+    return raiz(n, meio+1, alto);
+}
+
+int main(){
+    int n=20;
+    printf("%d\n", raiz(n,0,n));
+    return 0;
+}
