bdac60d5cb1db58f6dbffd6d93411c02aaa02a85
Amarganarada <131906487+EduardaMargonar06@users.noreply.github.com>
2025-09-08 17:35:42 -0300

    Create 18.c

---
+++ ATP_II/LISTA 5/18.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void hanoi(int n, char origem, char destino, char aux){
+    if(n==0) return;
+    hanoi(n-1, origem, aux, destino);
+    printf("Mover disco %d de %c para %c\n", n, origem, destino);
+    hanoi(n-1, aux, destino, origem);
+}
+
+int main(){
+    int n=3;
+    hanoi(n,'A','C','B');
+    return 0;
+}
