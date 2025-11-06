169398b5caeafa965b550c703adbc84625200ff6
Margo <131906487+EduardaMargonar06@users.noreply.github.com>
2025-11-05 22:03:44 -0300

    Add program to display month numbers

---
+++ ATP_II/LISTA 3/23.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+
+enum meses{
+    JANEIRO=1,
+    FEVEREIRO,
+    MARCO,
+    ABRIL,
+    MAIO,
+    JUNHO,
+    JULHO,
+    AGOSTO,
+    SETEMBRO,
+    OUTUBRO,
+    NOVEMBRO,
+    DEZEMBRO
+};
+
+int main()
+{
+    printf("JANEIRO: %d\n", JANEIRO);
+    printf("FEVEREIRO: %d\n", FEVEREIRO);
+    printf("MARCO: %d\n", MARCO);
+    printf("ABRIL: %d\n", ABRIL);
+    printf("MAIO: %d\n", MAIO);
+    printf("JUNHO: %d\n", JUNHO);
+    printf("JULHO: %d\n", JULHO);
+    printf("AGOSTO: %d\n", AGOSTO);
+    printf("SETEMBRO: %d\n", SETEMBRO);
+    printf("OUTUBRO: %d\n", OUTUBRO);
+    printf("NOVEMBRO: %d\n", NOVEMBRO);
+    printf("DEZEMBRO: %d\n", DEZEMBRO);
+
+    return 0;
+}
